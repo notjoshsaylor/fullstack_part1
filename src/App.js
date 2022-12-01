7541d472c9591cde9f6b03db905442a5c2580337
Joshua Saylor <joshuasaylor01@gmail.com>
2022-12-01 11:49:06 -0500

    full stack open ex. 1.8

--- src/App.js
+++ src/App.js
@@ -1,44 +1,49 @@
 import {useState} from "react"
-import Display from "./Display"
-import Button from "./Button"
-import History from "./History"
 
-const App =() => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  let all = good + neutral + bad
-  let average = (good + neutral + bad) / 3
-  let postitive = (good + neutral + bad) / good
- 
-  const handleGoodClick = () => {
-     setGood(good + 1)  
-  }
+const Button = ({onClick, body}) => <button onClick={onClick}>{body}</button>
 
-  const handleNeutralClick = () => {
-   setNeutral(neutral + 1)
-  }
-
-  const handleBadClick = () => {
-     setBad(bad + 1)
-  }
+const Stats = ({good, bad, neutral}) => {
+  const all = good + neutral + bad || 0
+  const average = (good + bad * -1) / all || 0
+  const positive = (all / good) * 100 || 0
 
   return(
     <>
-      <h1>Give FeedBack</h1>
-      <button onClick={handleGoodClick}>Good</button>
-      <button onClick={handleNeutralClick}>Neutral</button> 
-      <button onClick={handleBadClick}>bad</button> 
-      <h2>Statistics</h2>
+       <h2>Statistics</h2>
       <h4>Good: {good}</h4>
       <h4>Neutral: {neutral}</h4>
       <h4>Bad: {bad}</h4>
       <h4>All: {all}</h4>
       <h4>Average: {average}</h4>
-      <h4>Postitive: {postitive}</h4>
+      <h4>Postitive: {positive}</h4>
+    </>
+  )
+}
+
+const App =() => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleButton = (button) => () => {
+    if (button === 'good'){
+      setGood(good +1)
+    }else if (button === 'bad'){
+      setBad(bad + 1)
+      }else if (button === 'neutral'){
+        setNeutral(neutral + 1)
+      }
+    }
+
+  return(
+    <>
+      <h1>Give FeedBack</h1>
+      <Button style={{height: '30px', width:"30px"}} onClick={handleButton('good')} body={'good'} />
+      <Button style={{height: '30px'}} onClick={handleButton('neutral')} body={'neutral'} /> 
+      <Button style={{height: '30px'}} onClick={handleButton('bad')} body={'bad'} /> 
+      <Stats good={good} neutral={neutral} bad={bad}/>
     </>
     )
-  
 }
 
 
