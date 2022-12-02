97112c31527dce06b314d30c05052f2d1de78fd3
Joshua Saylor <joshuasaylor01@gmail.com>
2022-12-02 10:13:16 -0500

    full stack open ex. 1.12

--- src/App.js
+++ src/App.js
@@ -1,72 +1,31 @@
 import {useState} from "react"
 
-const Button = ({onClick, body}) => <button onClick={onClick}>{body}</button>
-const All = ({all}) => <div>All {all}</div>
-const Average = ({average}) => <div>Average {average}</div>
-const Positive = ({positive}) => <div>Positive {positive}</div>
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+  
 
-const Averages = ({all, average, positive}) => (
-  <>
-    <h2>Averages</h2>
+  const handleClick = () => {
+    const getRandom = Math.floor(Math.random() * anecdotes.length)
+    setSelected(getRandom)
+    }
+  
+  const [selected, setSelected] = useState(0)
+
+  return (
     <div>
-      <All all={all} value={all} />
-      <Average average={average} />
-      <Positive positive={positive} />
+      {anecdotes[selected]}
+      <br></br>
+      <button onClick={handleClick}>Click Here</button>
     </div>
-  </>
-)
-
-const Statistics = ({title, value}) => <div>{title} {value}</div>
-
-const Stats = ({good, bad, neutral}) => {
-  const all = good + neutral + bad || 0
-  const average = (good + bad + neutral) / 3 || 0
-  const positive = (good / all) * 100  || 0
-
-  if (all === 0 ){
-    return(
-      <p>No FeedBack given</p>
-    )
-  }
-
-  return(
-    <>
-      <h2>Statistics</h2>
-      <Statistics value={good} title={"Good"} />
-      <Statistics value={neutral} title={"Neutral"} />
-      <Statistics value={bad} title={"Bad"} />
-      <Averages average={average} all={all} positive={positive}/>
-    </>
   )
 }
-
-const App =() => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const handleButton = (button) => () => {
-    if (button === 'good'){
-      setGood(good +1)
-    }else if (button === 'bad'){
-      setBad(bad + 1)
-      }else if (button === 'neutral'){
-        setNeutral(neutral + 1)
-      }
-    }
-
-  return(
-    <>
-      <h1>Give FeedBack</h1>
-      <Button style={{height: '30px', width:"30px"}} onClick={handleButton('good')} body={'good'} />
-      <Button style={{height: '30px'}} onClick={handleButton('neutral')} body={'neutral'} /> 
-      <Button style={{height: '30px'}} onClick={handleButton('bad')} body={'bad'} /> 
-      <Stats good={good} neutral={neutral} bad={bad}/>
-    </>
-    )
-}
-
-
-
 
 export default App
