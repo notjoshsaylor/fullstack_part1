cba309aac7ea93e54f96d0f25afc69661513705a
Joshua Saylor <joshuasaylor01@gmail.com>
2022-12-02 11:32:01 -0500

    full stack open ex. 1.14

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {useState} from "react"
 
 const App = () => {
+  
   const anecdotes = [
     'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
@@ -11,19 +12,47 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
   ]
   
+  const [selected, setSelected] = useState(0)
+
+  const [points, setPoints] = useState({
+    0:0,
+    1:0,
+    2:0,
+    3:0,
+    4:0,
+    5:0
+  })
+
+  const [maxVote, setMaxVote] = useState(0)
 
   const handleClick = () => {
-    const getRandom = Math.floor(Math.random() * anecdotes.length)
+    const getRandom = Math.floor(Math.random() * anecdotes.length) +1
     setSelected(getRandom)
     }
-  
-  const [selected, setSelected] = useState(0)
+
+    const handleVote = () => {
+      setPoints({...points, [selected]: points[selected] += 1})
+      mostVotes()
+    }
+    const mostVotes = () => {
+      let max = 0
+
+      for(const point in points){
+        if(points[point] > max){
+          max = points[point]
+          setMaxVote(point)
+        }
+      }
+    }
 
   return (
     <div>
-      {anecdotes[selected]}
-      <br></br>
-      <button onClick={handleClick}>Click Here</button>
+      <h1>{anecdotes[selected]}</h1>
+      <h2>Has {points[selected]} votes</h2>
+      <button onClick={handleVote}>Vote</button>
+      <button onClick={handleClick}>New Anecdote</button>
+      <h2>Anecdote with the most votes</h2>
+      <h3>{anecdotes[maxVote]}</h3>
     </div>
   )
 }
